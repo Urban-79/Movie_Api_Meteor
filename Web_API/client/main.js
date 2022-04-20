2467d62dd314da50240b5c80ab97ced52d38194b
Mael <pydima@gmail.com>
2022-04-20 14:38:18 +0200

    new

--- Web_API/client/main.js
+++ Web_API/client/main.js
@@ -3,23 +3,19 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.home.onCreated(function homeOnCreated() {
   // counter starts at 0
+  let ctrl = this;
+  this.movies = new ReactiveVar();
   this.counter = new ReactiveVar(0);
-});
-
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-    HTTP.call('GET', 'http://localhost:3000/api/test', {}, function(error, response) {
-      console.log(response);
+  HTTP.call('GET', 'http://localhost:3000/api/discover/movie', {},
+    function(error, response) {
+      ctrl.movies.set(JSON.parse(response.content).results)
     });
+  });
+
+Template.home.helpers({
+  counter() {
+    return Template.instance().movies.get();
   },
 });
