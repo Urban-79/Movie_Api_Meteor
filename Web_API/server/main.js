b78e60f2bbe7d011dfd7c31e965336aa752f62e8
Dirbeq <dirbeq@gmail.com>
2022-04-20 15:44:07 +0300

    pomme

--- Web_API/server/main.js
+++ Web_API/server/main.js
@@ -2,14 +2,14 @@ import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';
 import { Mongo } from 'meteor/mongo';
 import { HTTP } from 'meteor/http';
-import { localDatas} from "./localDatas.js";
+ //import { localDatas} from "./localDatas.js";
 
 Meteor.startup(() => {
   // code to run on server at startup
     console.log("HELLO");
 });
 
-WebApp.connectHandlers.use('/api/discover/movie', (req, res, next) => {
+/*WebApp.connectHandlers.use('/api/discover/movie', (req, res, next) => {
   res.writeHead(200);
   res.end(JSON.stringify(localDatas));
-});
+});*/
