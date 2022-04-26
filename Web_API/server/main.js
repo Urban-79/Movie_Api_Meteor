4224fdcd212b446ac59fcbe1d652ac0768ecb53c
Dirbeq <dirbeq@gmail.com>
2022-04-26 16:58:22 +0200

    commentaires

--- Web_API/server/main.js
+++ Web_API/server/main.js
@@ -17,22 +17,24 @@ Meteor.startup(() => {
 function updateLikeMovie(idMovie) {
 
 
-
+  //On prend le row du film
   let dbRessource = film.findOne({ id: idMovie });
 
 
-
+  //Si il existe
   if (dbRessource) {
+    //On prend l'id du row et on increase son like de 1
     film.update(
       { _id: dbRessource._id },
       { $inc: { like: 1 } }
     );
   } else {
-    film.insert({ id: idMovie, like: 1 }); //Mettre le insert mongoDB ici
+    //Si le row n'existe pas on l'ajoute
+    film.insert({ id: idMovie, like: 1 }); 
   }
 
 
-
+  //On retourne un truck
   return film.findOne({ id: idMovie });
   // return {id: film.findOne({ id: idMovie }).id, like: film.findOne({ id: idMovie }).like}
 }
