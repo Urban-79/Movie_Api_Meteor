9e2736db3ed301cf62f4d5cb685881f96a13c257
Dirbeq <dirbeq@gmail.com>
2022-04-26 16:34:58 +0200

    serverConfig & la fonction updateLikeMovie

--- Web_API/server/main.js
+++ Web_API/server/main.js
@@ -13,3 +13,26 @@ Meteor.startup(() => {
   res.writeHead(200);
   res.end(JSON.stringify(localDatas));
 });*/
+
+function updateLikeMovie(idMovie){
+
+
+
+  let dbRessource = LikesCollection.findOne({ id: idMovie });
+  
+  
+  
+  if(dbRessource) {
+  LikesCollection.update(
+  { _id: dbRessource._id },
+  { $inc: {like: 1} }
+  );
+  } else {
+  LikesCollection.insert({id: idMovie, like: 1}); //Mettre le insert mongoDB ici
+  }
+  
+  
+  
+  return LikesCollection.findOne({ id: idMovie });
+  // return {id: LikesCollection.findOne({ id: idMovie }).id, like: LikesCollection.findOne({ id: idMovie }).like}
+  }
