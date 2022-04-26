8d9d1fe2b68c4723db1ddcc1208aa16629311d83
Dirbeq <dirbeq@gmail.com>
2022-04-26 16:36:30 +0200

    format'

--- Web_API/server/main.js
+++ Web_API/server/main.js
@@ -2,11 +2,11 @@ import { Meteor } from 'meteor/meteor';
 import { WebApp } from 'meteor/webapp';
 import { Mongo } from 'meteor/mongo';
 import { HTTP } from 'meteor/http';
- //import { localDatas} from "./localDatas.js";
+//import { localDatas} from "./localDatas.js";
 
 Meteor.startup(() => {
   // code to run on server at startup
-    console.log("HELLO");
+  console.log("HELLO");
 });
 
 /*WebApp.connectHandlers.use('/api/discover/movie', (req, res, next) => {
@@ -14,25 +14,25 @@ Meteor.startup(() => {
   res.end(JSON.stringify(localDatas));
 });*/
 
-function updateLikeMovie(idMovie){
+function updateLikeMovie(idMovie) {
 
 
 
   let dbRessource = LikesCollection.findOne({ id: idMovie });
-  
-  
-  
-  if(dbRessource) {
-  LikesCollection.update(
-  { _id: dbRessource._id },
-  { $inc: {like: 1} }
-  );
+
+
+
+  if (dbRessource) {
+    LikesCollection.update(
+      { _id: dbRessource._id },
+      { $inc: { like: 1 } }
+    );
   } else {
-  LikesCollection.insert({id: idMovie, like: 1}); //Mettre le insert mongoDB ici
+    LikesCollection.insert({ id: idMovie, like: 1 }); //Mettre le insert mongoDB ici
   }
-  
-  
-  
+
+
+
   return LikesCollection.findOne({ id: idMovie });
   // return {id: LikesCollection.findOne({ id: idMovie }).id, like: LikesCollection.findOne({ id: idMovie }).like}
-  }
+}
