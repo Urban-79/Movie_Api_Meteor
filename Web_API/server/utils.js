2c5d1541dc400c7955c82bc9902ccea119e743b7
Dirbeq <dirbeq@gmail.com>
2022-04-27 14:09:29 +0200

    ajoutLikeDansMangoFonctionnel

--- Web_API/server/utils.js
+++ Web_API/server/utils.js
@@ -10,28 +10,3 @@ function movieByUrl(key) {
     console.log(url);
     return url;
 }
-
-function updateLikeMovie(idMovie) {
-
-
-    //On prend le row du film
-    let dbRessource = film.findOne({ id: idMovie });
-
-
-    //Si il existe
-    if (dbRessource) {
-        //On prend l'id du row et on increase son like de 1
-        film.update(
-            { _id: dbRessource._id },
-            { $inc: { like: 1 } }
-        );
-    } else {
-        //Si le row n'existe pas on l'ajoute
-        film.insert({ id: idMovie, like: 1 });
-    }
-
-
-    //On retourne un truck
-    return film.findOne({ id: idMovie });
-    // return {id: film.findOne({ id: idMovie }).id, like: film.findOne({ id: idMovie }).like}
-}
